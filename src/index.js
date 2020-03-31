9bdf17d8180b42f3eab80299d6b0c0d0b58af3d5
mxburton <51886361+mxburton@users.noreply.github.com>
2020-03-31 16:53:23 +0700

    init commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import "core-js/features/map";
+import "core-js/features/set";
+import React from "react";
+import ReactDOM from "react-dom";
+import bridge from "@vkontakte/vk-bridge";
+import App from "./App";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// Init VK  Mini App
+bridge.send("VKWebAppInit");
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<App />, document.getElementById("root"));
+if (process.env.NODE_ENV === "development") {
+  import("./eruda").then(eruda => {}); //runtime download
+}
